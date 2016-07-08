88b866f1545d57f9c1742e4dac59ff6c8b9ebe9b
yasin <1334036616@qq.com>
2016-07-08 15:36:19 +0800

    创建项目

---
+++ app/src/main/java/yasin/com/qustlogin/MainActivity.java
@@ -0,0 +1,13 @@
+package yasin.com.qustlogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
